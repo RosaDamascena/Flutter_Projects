4493423eef8e49ad1f572c21468186c2dd343979
RosaDamascena <hsj990604@gmail.com>
2023-01-09 03:20:32 +0900

    feat : Add function buttons

--- financialapp/lib/main.dart
+++ financialapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:financialapp/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +10,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 30
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
               Row(
@@ -26,63 +27,62 @@ class MyApp extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text('Hey, DamaskRose',
+                      const Text(
+                        'Hey, DamaskRose',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 24,
                           fontWeight: FontWeight.w700,
                         ),
                       ),
-                      Text('Welcome back',
+                      Text(
+                        'Welcome back',
                         style: TextStyle(
                           color: Colors.white.withOpacity(0.8),
                           fontSize: 18,
                         ),
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
-              SizedBox(height: 120,
+              const SizedBox(
+                height: 120,
               ),
-              Text('Total Balance',
+              Text(
+                'Total Balance',
                 style: TextStyle(
                   fontSize: 22,
                   color: Colors.white.withOpacity(0.8),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text('\$5 194 482',
+              const Text(
+                '\$5 194 482',
                 style: TextStyle(
                   fontSize: 44,
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF2B33A),
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 50,
-                      ),
-                      child: Text('Transfer',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  )
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF2B33A),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
                 ],
               ),
             ],

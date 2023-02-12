b80ec8bf61e241dce95433efc8a3b7eec9391d0c
RosaDamascena <hsj990604@gmail.com>
2023-02-13 03:30:22 +0900

    feat : Create itoon App that shows today's webtoon list

--- itoon/lib/main.dart
+++ itoon/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:itoon/screens/home_screen.dart';
+import 'package:itoon/services/api_service.dart';
 
 void main() {
-  runApp(MyApp());
+  ApiService().getTodaysToons();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
